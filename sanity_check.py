2ddf56e42506c8b22f035d4eb4edb9ac7a6ebfe3
Shuyan Zhou <alexisxy0418@gmail.com>
2021-09-26 23:52:58 -0400

    update sanity check

--- sanity_check.py
+++ sanity_check.py
@@ -10,6 +10,11 @@ att_mask = torch.tensor([[1, 1, 1, 1, 0, 0, 0, 0],[1, 1, 1, 1, 1, 1, 1, 1]])
 # load our model
 bert = BertModel.from_pretrained('bert-base-uncased')
 outputs = bert(sent_ids, att_mask)
+att_mask = att_mask.unsqueeze(-1)
+outputs['last_hidden_state'] = outputs['last_hidden_state'] * att_mask
+sanity_data['last_hidden_state'] = sanity_data['last_hidden_state'] * att_mask
+
 for k in ['last_hidden_state', 'pooler_output']:
-    assert torch.allclose(outputs[k], sanity_data[k], rtol=1e-3)
+    assert torch.allclose(outputs[k], sanity_data[k], atol=1e-5, rtol=1e-3)
 print("Your BERT implementation is correct!")
+
